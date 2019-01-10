b7387cb9edbf5e06a9a3576e5b8e29dbe93e8e62
Bastian Müller <bastian@turbolent.com>
2019-01-10 12:16:48 -0800

    move from com.bigdata to org.embergraph

--- embergraph-rdf-test/src/test/java/org/embergraph/rdf/sparql/ast/eval/TestDefaultGraphs.java
+++ embergraph-rdf-test/src/test/java/org/embergraph/rdf/sparql/ast/eval/TestDefaultGraphs.java
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * Created on Nov 19, 2011
  */
 
-package com.bigdata.rdf.sparql.ast.eval;
+package org.embergraph.rdf.sparql.ast.eval;
 
 import com.bigdata.bop.BOp;
 import com.bigdata.bop.IPredicate;
